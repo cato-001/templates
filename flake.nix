d5444295fee38f781803b6a4856a5bc5d17260b0
cato-001 <catowbr001@gmail.com>
2026-01-02 11:31:22 +0100

    feat: add nix flake

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "My common project templates";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.11";
+
+    blueprint = {
+      url = "github:numtide/blueprint";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs: inputs.blueprint { inherit inputs; };
+}
